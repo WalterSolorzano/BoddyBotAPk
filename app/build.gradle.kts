669f3b4bde9345fb86615bfd4c14fa1f15512968
Walter Noel Solorzano Gaitan <49598410+WalterSolorzano@users.noreply.github.com>
2026-07-20 00:19:59 -0600

    build: add MAPS_API_KEY placeholder to gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ android {
     versionName = "1.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    manifestPlaceholders["MAPS_API_KEY"] = "TU_API_KEY_AQUI"
   }
 
   buildTypes {
